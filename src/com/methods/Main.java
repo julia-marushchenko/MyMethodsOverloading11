fb803e06fecec4bf5fc06bed742e36d6f8688e8a
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-07-12 15:36:47 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/methods/Main.java
@@ -0,0 +1,47 @@
+// Java program to demonstrate methods overloading
+
+package com.methods;
+
+// Main class
+public class Main {
+
+    // Method get to find difference of two integers
+    public int substruction(int number, int number1){
+        int result = number - number1;
+        return result;
+    }
+
+    // Method get to  find difference of three integers
+    public int substruction(int number, int number1, int number2){
+        int result = number - number1 - number2;
+        return result;
+    }
+
+    // Method get to find difference of two doubles
+    public double substruction(double number, double number1){
+        double result = number - number1;
+        return result;
+    }
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating Main instance
+        Main m = new Main();
+
+        // Substruction of two integers
+        int sub = m.substruction(14, 17);
+        // Printing to console the result
+        System.out.println(sub);
+
+        // Substruction of three integers
+        int sub1 = m.substruction(14, 8, 2);
+        // Printing to console the result
+        System.out.println(sub1);
+
+        // Substruction of two doubles
+        double sub2 = m.substruction(14.7, 11.4);
+        // Printing to console the result
+        System.out.println(sub2);
+    }
+}
